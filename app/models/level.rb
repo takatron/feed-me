cbbab830ac9ddf347e96ef9b49c673526a2f9178
Takara <takara@influitive.com>
2019-09-21 16:45:43 -0400

    adds levels migration and seed

---
+++ app/models/level.rb
@@ -0,0 +1,4 @@
+class Level < ActiveRecord::Base
+  validates    :name, presence: true, length: { in: 3..20 }, uniqueness: true
+  has_many     :recipes
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+levels = [{ name: 'Easy' }, { name: 'Medium' }, { name: 'Hard' }]
+Level.create(levels)

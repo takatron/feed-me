17d0200771f5685f66b0eee4f3f56f8c5f8810ef
Duy Ngan Thuy Tran <takatron@TNTs-MacBook-Pro.local>
2019-10-13 11:39:17 -0400

    category validation force lowercase for colour

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
+  validates    :colour, length: { in: 3..20 }, uniqueness: true
+  
   has_many   :categories_recipes
   has_many   :recipes, through: :categories_recipes
+
+  before_save { colour.downcase! }
 end
